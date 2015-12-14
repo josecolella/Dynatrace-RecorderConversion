604f745291a7db604784a49f6d15436f11fa8953
josecolella <josecolella@yahoo.com>
2015-12-14 22:57:15 +0100

    Create commandline program for easy conversion

--- DynatraceRecorderConverter.py
+++ DynatraceRecorderConverter.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Author: Jose Miguel Colella
+# email: jose.colella@dynatrace.com
 import base64
 import json
 import itertools
@@ -63,12 +65,39 @@ class _JSONConverter(object):
             doPageSummary="false",
             name=self.jsonContents["name"]
         )
+        # Method for adding user agent info if in json script
+        self._createUserAgentSection()
         # Method for creating the configuration parameters section
         self._createConfigurationSection()
         # Method for creating the steps section
         self._createStepSection()
         # Add pre_script attr to first node
         self._addPreScriptAttribute()
+
+    def _createUserAgentSection(self):
+
+        if self.jsonContents.get("userAgent") is not None:
+            headerString = "http://www.gomez.com/headers"
+            # Create configuration node
+            configurationNode = etree.Element(
+                "Configuration", name=headerString)
+            # Create Param node
+            parameter = etree.Element(
+                "Param", name="User-Agent", value="{}".format(self.jsonContents["userAgent"]))
+            # Append Param node to configuration node
+            configurationNode.append(parameter)
+            # Append Configuration node to root Transaction node
+            self.rootNode.append(configurationNode)
+
+    def _createDeadlineNodesIter(self):
+        deadlineNodes = ()
+        deadline = {
+            "hardDeadline": "http://www.gomez.com/settings/hard_deadline",
+            "softDeadline": "http://www.gomez.com/settings/soft_deadline"
+        }
+        deadlineNodes = (etree.Element("Param", name="{}".format(pvalue), value="{}".format(self.jsonContents[key]))
+                         for key, pvalue in deadline.items() if self.jsonContents.get(key) is not None)
+        return deadlineNodes
 
     def _createConfigurationSection(self):
         """Creates and appends all the test configuration and creates all
@@ -79,7 +108,7 @@ class _JSONConverter(object):
         # The configuration dictionary including flash and Scoe settings
         configDict = {
             "enableFlash": "http://www.gomez.com/capabilities/enable_flash",
-            "enableScoe": "http://www.gomez.com/capabilities/enable_scoe"
+            "enableScoe": "http://www.gomez.com/capabilities/enable_scoe",
         }
         # Network settings
         networkSettings = {"ipMode": "http://www.gomez.com/settings/ip_mode"}
@@ -93,10 +122,26 @@ class _JSONConverter(object):
         settings = (etree.Element("Param", name="{}".format(configuration["name"]), value="{}".format(
             configuration["value"])) for configuration in self.jsonContents["configurations"])
         # Create an iterator with all the <Param> tags
-        parameterIterator = itertools.chain(capabilities, network, settings)
+        parameterIterator = itertools.chain(
+            capabilities, self._createDeadlineNodesIter(), network, settings)
         # Append the tags to the root node
         for parameter in parameterIterator:
             configurationNode.append(parameter)
+
+    def _createSubstitutionNodesIter(self, step):
+        # Given a step returns a Substitution node list
+        substitutionNodes = None
+
+        if step.get("substitutions") is not None:
+
+            substitutionNodes = (etree.Element("Substitution",
+                                               selector="{}".format(
+                                                   substitution["selector"]),
+                                               token="{}".format(
+                                                   substitution["token"]),
+                                               value="{}".format(substitution["value"]))
+                                 for substitution in step["substitutions"])
+        return substitutionNodes
 
     def _createStepSection(self):
         """Creates and appends all the test steps into the definition of the gsl
@@ -114,6 +159,16 @@ class _JSONConverter(object):
                                                 postScript.decode("utf-8")),
                                             url=step["url"]
                                             )
+
+            if step.get("softDeadline") or step.get("hardDeadline"):
+                configurationNode = etree.Element("Configuration")
+                for deadlineNode in self._createDeadlineNodesIter():
+                    configurationNode.append(deadlineNode)
+                pageRequestNode.append(configurationNode)
+            substitutionNodes = self._createSubstitutionNodesIter(step)
+            if substitutionNodes is not None:
+                for substititutionNode in substitutionNodes:
+                    pageRequestNode.append(substititutionNode)
             self.rootNode.append(pageRequestNode)
 
     def _addPreScriptAttribute(self):
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from DynatraceRecorderConverter import *
+import argparse
+from DynatraceRecorderConverter import DynatraceRecorderConverter
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog="Dynatrace Recorder Converter")
+    parser.add_argument(
+        "-i", "--input", help="The name of the input file", type=str, required=True)
+    parser.add_argument(
+        "-o", "--output", help="The name of the output file", type=str, default="tmp")
+    parser.add_argument(
+        "-v", "--verbose", help="Display debug message", action="store_true")
+    args = parser.parse_args()
     DynatraceRecorderConverter.convertJSONFileToGSLFile(
-        "MINGP_Sparen_Tonen-spaargegevens.json", "MINGP_Sparen_Tonen-spaargegevens")
+        args.input, args.output)
